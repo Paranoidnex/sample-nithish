3387e3a8118b1a8ab2d1d99199267ee4e81cf890
Paranoidnex <paranoidnex999@gmail.com>
2025-08-24 17:38:40 +0530

    python app.py

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
-print('welcome')
+from flask import flask 
 
+app =flask(__name__)
+
+@app.route('/welcome')
+def hello():
+  return "jenkins triggered successfully!"
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=80)
