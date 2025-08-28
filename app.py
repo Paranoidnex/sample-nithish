bca3230ac7a99e1aab439571617c71b33cc285b3
Paranoidnex <paranoidnex999@gmail.com>
2025-08-28 20:09:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app =flask(__name__)
 
 @app.route('/welcome')
 def hello():
-  return "everythings working phhh!"
+  return "everythings working phh!"
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=80)
